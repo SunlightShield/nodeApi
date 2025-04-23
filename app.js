1c054d84f844685dbda57e9236860ea0e3a449ab
Sunlight <sespinozahenriquez@gmail.com>
2025-04-23 19:19:52 -0400

    ajustes

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const port = process.env.PORT || 4000;
 
 // Middlewares
 app.use(bodyParser.json());
-
+const cors = require('cors');
+app.use(cors({
+origin: 'http://localhost:3000'
+}));
 // Rutas
 app.use('/api/abogados', require('./routes/abogados'));
 app.use('/api/contactos', require('./routes/contactos'));
